ddf3265c6ed9b2624f638aa1d0bddcb80d5810dd
wellcomemayhem-spec <hirajfaryar@hotmail.com>
2025-08-30 13:38:10 +0800

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ColorPicker from './ColorPicker';
+
+function App() {
+    return (
+        <div className="app">
+            <h1>Color Picker Tool</h1>
+            <ColorPicker />
+        </div>
+    );
+}
+
+export default App;
